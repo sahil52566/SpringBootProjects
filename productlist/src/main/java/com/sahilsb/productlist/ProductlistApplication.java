c0a83e3a9ae22d38f95ca26ec6738cb865a9bcaf
sahil52566 <sahilsingh52566@gmail.com>
2025-03-09 01:28:22 +0530

    productlist

---
+++ productlist/src/main/java/com/sahilsb/productlist/ProductlistApplication.java
@@ -0,0 +1,13 @@
+package com.sahilsb.productlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlistApplication.class, args);
+	}
+
+}
